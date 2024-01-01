d142e9f8aca4814792a12bf43a8a18378c95c0c2
Ernestolop <ernestodaniellopez504@gmail.com>
2024-01-01 19:46:52 -0300

    feat:addProductCard

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,4 @@ export { default as TopMenu } from './ui/top-meu/TopMenu';
 export { default as NotFound } from './ui/not-found/NotFound';
 export { default as Title } from './ui/title/Title';
 export { default as ProductsGrid } from './products/products-grid/ProductsGrid';
+export { default as ProductGridItem } from './products/products-grid/ProductGridItem';
