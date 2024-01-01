0f18632c7c39246cf4c94ea37c75e79790be42f6
Ernestolop <ernestodaniellopez504@gmail.com>
2024-01-01 19:55:00 -0300

    feat:addChangeImageOnClick

--- src/components/products/products-grid/ProductGridItem.jsx
+++ src/components/products/products-grid/ProductGridItem.jsx
@@ -1,16 +1,24 @@
+'use client';
+
+import { useState } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 
 const ProductGridItem = ({ product }) => {
+
+    const [displayImage, setDisplayImage] = useState(product.images[0]);
+
     return (
         <div className="rounded-md overflow-hidden fade-in">
             <Link href={`/product/${product.slug}`}>
                 <Image
-                    src={`/products/${product.images[0]}`}
+                    src={`/products/${displayImage}`}
                     width={500}
                     height={500}
                     alt={product.title}
-                    className='w-full object-cover'
+                    className='w-full object-cover rounded'
+                    onMouseEnter={() => setDisplayImage(product.images[1])}
+                    onMouseLeave={() => setDisplayImage(product.images[0])}
                 />
             </Link>
             <div className='p-4 flex flex-col'>
