4b297cde9ba07e7f87f91e0ca4d229d3d4b9b6a1
Ernestolop <ernestodaniellopez504@gmail.com>
2024-01-01 20:43:47 -0300

    feat:addMenuSidebar

--- src/app/(shop)/layout.jsx
+++ src/app/(shop)/layout.jsx
@@ -1,9 +1,10 @@
-import { TopMenu } from "@/components";
+import { TopMenu, Sidebar } from "@/components";
 
 const ShopLayout = ({ children }) => {
     return (
         <main className="min-h-screen">
             <TopMenu />
+            <Sidebar />
             <div className="px-0 sm:px-10">
                 {children}
             </div>
--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,4 @@ export { default as NotFound } from './ui/not-found/NotFound';
 export { default as Title } from './ui/title/Title';
 export { default as ProductsGrid } from './products/products-grid/ProductsGrid';
 export { default as ProductGridItem } from './products/products-grid/ProductGridItem';
+export { default as Sidebar } from './ui/sidebar/Sidebar';
