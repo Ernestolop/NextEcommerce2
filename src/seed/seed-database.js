5ad07d1f0f08f1f64dcf2eb531ddd7d883e30b7b
Ernestolop <ernestodaniellopez504@gmail.com>
2024-02-08 20:37:49 -0300

    feat: add seed creates and deletes

--- src/seed/seed-database.js
+++ src/seed/seed-database.js
@@ -1,10 +1,55 @@
+import prisma from '../lib/prisma.mjs'
 import { initialData } from './seed.js';
 
 const main = async () => {
-    console.log(initialData);
+
+    //1- Borrar registros previos
+    await Promise.all([
+        prisma.productImage.deleteMany(),
+        prisma.product.deleteMany(),
+        prisma.category.deleteMany()
+    ]);
+
+    const { categories, products } = initialData;
+
+    //Categoria
+    const categoriesData = categories.map(name => ({ name }));
+    await prisma.category.createMany({ data: categoriesData });
+
+    const categoriesDB = await prisma.category.findMany();
+    const categoriesMap = categoriesDB.reduce((map, category) => {
+        map[category.name] = category.id;
+        return map;
+    }, {});
+
+    //Productos
+    products.forEach(async product => {
+        const { images, type, ...productData } = product;
+        const categoryId = categoriesMap[type];
+
+        const productDb = await prisma.product.create({
+            data: {
+                ...productData,
+                categoryId
+            }
+        });
+
+        //Imagenes
+        const imagesData = images.map(url => ({
+            url,
+            productId: productDb.id
+        }));
+
+        await prisma.productImage.createMany({ data: imagesData });
+
+    });
+
+
+    console.log('Seed ejecutado con éxito');
+
 }
 
 (() => {
-    if(process.env.NODE_ENV === 'production') return;
+    if (process.env.NODE_ENV === 'production') return;
     main();
 })();
--- src/seed/seed.js
+++ src/seed/seed.js
@@ -1,4 +1,10 @@
 export const initialData = {
+    categories: [
+        'shirts',
+        'hoodies',
+        'hats',
+        'pants'
+    ],
     products: [
         {
             description: "Introducing the Tesla Chill Collection. The Men’s Chill Crew Neck Sweatshirt has a premium, heavyweight exterior and soft fleece interior for comfort in any season. The sweatshirt features a subtle thermoplastic polyurethane T logo on the chest and a Tesla wordmark below the back collar. Made from 60% cotton and 40% recycled polyester.",
